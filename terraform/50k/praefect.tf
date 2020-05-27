c86420ba98ad15f83c08373002784d84871472ce
Grant Young <gyoung@gitlab.com>
2020-05-27 10:16:00 +0100

    Separate DB for praefect
    Just to test out that scenario

--- terraform/50k/praefect.tf
+++ terraform/50k/praefect.tf
@@ -5,11 +5,27 @@ module "praefect" {
   node_type = "praefect"
   node_count = 3
 
-  machine_type = "n1-highcpu-2"
+  machine_type = "n1-highcpu-16"
   machine_image = "${var.machine_image}"
   label_secondaries = true
 }
 
 output "praefect" {
   value = module.praefect
+}
+
+module "praefect_postgres" {
+  source = "../modules/gitlab_gcp_instance"
+
+  prefix = "${var.prefix}"
+  node_type = "praefect-postgres"
+  node_count = 1
+
+  machine_type = "n1-standard-8"
+  machine_image = "${var.machine_image}"
+  label_secondaries = true
+}
+
+output "praefect_postgres" {
+  value = module.praefect_postgres
 }
