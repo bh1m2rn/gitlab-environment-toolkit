3214e0e52118c4eb8c6c09e72739b4c1b64690c4
nwestbury <nwestbury@gitlab.com>
2020-09-02 11:36:38 +0100

    Add missing vars to internal haproxy

--- terraform/3k_geo/primary/haproxy.tf
+++ terraform/3k_geo/primary/haproxy.tf
@@ -25,6 +25,9 @@ module "haproxy_internal" {
   prefix = "${var.prefix}"
   node_type = "haproxy-internal"
   node_count = 1
+
+  geo_role = "${var.geo_role}"
+  geo_group = "${var.geo_group}"
   
   machine_type = "n1-highcpu-2"
   machine_image = "${var.machine_image}"
