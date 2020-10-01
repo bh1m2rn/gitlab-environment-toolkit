9f5d2d50b90a1890cf79c89be1db381c4817c450
nwestbury <nwestbury@gitlab.com>
2020-10-01 16:22:04 +0100

    add new label for geo

--- terraform/modules/gitlab_gcp_instance/main.tf
+++ terraform/modules/gitlab_gcp_instance/main.tf
@@ -28,6 +28,7 @@ resource "google_compute_instance" "gitlab" {
     gitlab_node_level = var.label_secondaries == true ? (count.index == 0 ? "${var.node_type}-primary" : "${var.node_type}-secondary") : ""
     gitlab_geo_role = var.geo_role
     gitlab_geo_group = var.geo_group
+    gitlab_geo_full_role = var.geo_role == null ? null : (count.index == 0 ? "${var.geo_role}-${var.node_type}-primary" : "${var.geo_role}-${var.node_type}-secondary")
   }
 
   network_interface {
