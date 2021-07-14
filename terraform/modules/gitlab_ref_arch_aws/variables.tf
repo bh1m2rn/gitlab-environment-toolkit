0c73fdd255a2f9ec659962ba7fce680a5bab70c2
Cristiano Casella <cristiano.casella@gmail.com>
2021-07-14 19:13:03 +0200

    creating first resources

--- terraform/modules/gitlab_ref_arch_aws/variables.tf
+++ terraform/modules/gitlab_ref_arch_aws/variables.tf
@@ -105,3 +105,9 @@ variable "sidekiq_node_count" { default = 0 }
 variable "sidekiq_instance_type" { default = "" }
 variable "sidekiq_disk_type" { default = null }
 variable "sidekiq_disk_size" { default = null }
+
+# Networking
+variable "vpc_cidr_block" { default = "172.31.0.0/16" }
+variable "subpub1_cidr_block" { default = "172.31.0.0/20" }
+variable "subpub2_cidr_block" { default = "172.31.16.0/20" }
+variable "subpub3_cidr_block" { default = "172.31.32.0/20" }
