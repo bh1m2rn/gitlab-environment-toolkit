9913652630f2605dd11cee53b7f55a8b5263d45b
Cristiano Casella <cristiano.casella@gmail.com>
2021-07-19 08:39:49 +0200

    moving security groups to dedicated file

--- terraform/modules/gitlab_ref_arch_aws/security.tf
+++ terraform/modules/gitlab_ref_arch_aws/security.tf
@@ -2,3 +2,99 @@ resource "aws_key_pair" "ssh_key" {
   key_name   = "${var.prefix}-ssh-key"
   public_key = var.ssh_public_key_file
 }
+
+resource "aws_security_group" "gitlab_internal_networking" {
+  # Allows for machine internal connections as well as outgoing internet access
+  name = "${var.prefix}-internal-networking"
+  vpc_id = data.aws_vpc.selected.id
+
+  ingress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    self = true
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "gitlab_external_ssh" {
+  name = "${var.prefix}-external-ssh"
+  vpc_id = data.aws_vpc.selected.id
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "gitlab_external_git_ssh" {
+  name = "${var.prefix}-external-git-ssh"
+  vpc_id = data.aws_vpc.selected.id
+  ingress {
+    from_port   = 2222
+    to_port     = 2222
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "gitlab_external_http_https" {
+  name = "${var.prefix}-external-http-https"
+  vpc_id = data.aws_vpc.selected.id
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "gitlab_external_haproxy_stats" {
+  name = "${var.prefix}-external-haproxy-stats"
+  vpc_id = data.aws_vpc.selected.id
+  ingress {
+    from_port   = 1936
+    to_port     = 1936
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "gitlab_external_monitor" {
+  name = "${var.prefix}-external-monitor"
+  vpc_id = data.aws_vpc.selected.id
+  ingress {
+    from_port   = 9122
+    to_port     = 9122
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 9090
+    to_port     = 9090
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 5601
+    to_port     = 5601
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
