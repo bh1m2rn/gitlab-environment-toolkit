36744240ab4f1ee61870adda9996fbfc7d1d9c22
Vlad Stoianovici <vstoianovici@gitlab.com>
2021-07-25 22:05:05 +0000

    Changed network in firewall rules as well

--- terraform/modules/gitlab_ref_arch_gcp/firewall.tf
+++ terraform/modules/gitlab_ref_arch_gcp/firewall.tf
@@ -1,7 +1,7 @@
 resource "google_compute_firewall" "gitlab_http_https" {
   count = min(var.haproxy_external_node_count + var.monitor_node_count, 1)
   name = "${var.prefix}-gitlab-rails-firewall-rule-http-https"
-  network = "default"
+  network = "support-resources"
 
   description = "Allow external load balancer access"
 
@@ -20,7 +20,7 @@ resource "google_compute_firewall" "gitlab_http_https" {
 resource "google_compute_firewall" "gitlab_ssh" {
   count = min(var.haproxy_external_node_count, 1)
   name    = "${var.prefix}-gitlab-rails-firewall-rule-ssh"
-  network = "default"
+  network = "support-resources"
 
   description = "Allow access to GitLab SSH via external load balancer"
 
@@ -39,7 +39,7 @@ resource "google_compute_firewall" "gitlab_ssh" {
 resource "google_compute_firewall" "haproxy_stats" {
   count = min(var.haproxy_external_node_count + var.haproxy_internal_node_count, 1)
   name    = "${var.prefix}-haproxy-stats-firewall-rule"
-  network = "default"
+  network = "support-resources"
 
   description = "Allow HAProxy Stats access"
 
@@ -58,7 +58,7 @@ resource "google_compute_firewall" "haproxy_stats" {
 resource "google_compute_firewall" "monitor" {
   count = min(var.monitor_node_count, 1)
   name    = "${var.prefix}-monitor-firewall-rule"
-  network = "default"
+  network = "support-resources"
 
   description = "Allow Prometheus and InfluxDB exporter access"
 
